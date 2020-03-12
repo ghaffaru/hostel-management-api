a34f08afce0a977ca43d8fe2b3f4b5979bcc0f18
Ghaffaru <mudashiruagm@gmail.com>
2020-03-12 07:22:10 +0000

    connect remote mongo url

--- database/connect.js
+++ database/connect.js
@@ -1,8 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+if (process.env.NODE_ENV === "local") {
+  mongoose.connect(
+    "mongodb://" +
+      process.env.DB_HOST +
+      ":" +
+      process.env.DB_PORT +
+      "/" +
+      process.env.DB_NAME,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+} else if (process.env.NODE_ENV === "production") {
+  mongoose.connect(
+    "mongodb://" +
+      process.env.DB_USER_PROD +
+      ":" +
+      process.env.DB_PASS_PROD +
+      "@" +
+      process.env.DB_HOST_PROD +
+      ":" +
+      process.env.DB_PORT_PROD +
+      "/" +
+      process.env.DB_NAME_PROD
+  );
+}
 
 mongoose.Promise = global.Promise;
